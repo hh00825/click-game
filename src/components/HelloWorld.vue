521ced5e2db2866e0cd3853d77f5f7a22a586e85
Haejin Jeong <110663389+hh00825@users.noreply.github.com>
2025-02-27 17:53:36 +0900

    1차 작업 완료

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,142 @@
+
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+    <div class="inner">
+        <section class="section intro-section" v-show="step==1">
+            <div class="contents-box">
+                <div class="top-text-box">
+                    <p class="main-title">제한시간동안 {{buttonName}}를 마구마구 클릭하여 <br/>주어진 숫자를 만들어보세요.</p>
+                    <p class="main-title_desc">시간내에 숫자를 초과해도 탈락!</p>
+                </div>
+                <div class="middle-main-box">
+                    <div class="level-item">
+                        <span class="number">{{level}}</span><span class="text">단계</span>
+                    </div>
+                </div>
+            </div>
+            <div class="button-box">
+                <button @click="gameStart($event)" data-setting="init" class="button">게임 시작!</button>
+            </div>
+        </section>
+
+        <section class="section game-section" v-if="step==2">
+            <div class="contents-box">
+                <div class="top-text-box">
+                    <strong class="current-stage">{{level}} 단계</strong>
+                    <p class="main-title">아래 {{buttonName}}를 연타해서 <br/>숫자 {{goalCount}}를 만들어보세요!</p>
+                    <p class="main-title_desc">제한 시간 {{duration}}초!!</p>
+                </div>
+                <div class="middle-main-box">
+                    <div class="time-box">
+                        <p>남은 시간 <strong class="color-red">{{duration-currentTime}}초</strong></p>
+                    </div>
+                    <div class="white-box">
+                        <div class="count-box">
+                            <span>현재까지 누른 횟수</span>
+                            <strong>{{count}}</strong>
+                        </div>
+                        
+                        <div class="bubble-box">
+                            <p v-if="count == goalCount">숫자를 다 맞췄어요! <br/>제한 시간까지 잠시만 기다려주세요.</p>
+                            <p v-else-if="count >= goalCount-10">거의 다왔어요! {{goalCount-count}}개만 더!</p>
+                            <p v-else-if="count > goalCount/2">절반 왔어요. 조금만 힘내세요!</p>
+                            <p v-else>여기를 클릭하세요. 화이팅!!</p>
+                        </div>
+                        <button @click="counter()" class="event-button">
+                            <img :src="require('../assets/click-target-img01.jpg')" alt="{{buttonName}}">
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="section result-section" v-if="step=='3_success'">
+            <div class="contents-box">
+                <div class="top-text-box">
+                    <strong class="current-stage">{{level-1}} 단계</strong>
+                    <p class="main-title">성공!!</p>
+                    <div class="result-img">
+                        <img :src="require('../assets/success-img.jpg')" alt="성공">
+                    </div>
+                    <p class="main-title">축하합니다. <br/>다음단계로 넘어갑니다.</p>
+                </div>
+            </div>
+            <div class="button-box">
+                <button @click="gameStart($event)" data-setting="success" class="button">다음 단계로!</button>
+            </div>
+        </section>
+
+        <section class="section result-section" v-if="step=='3_fail'">
+            <div class="contents-box">
+                <div class="top-text-box">
+                    <strong class="current-stage">{{level}} 단계</strong>                    
+                    <p class="main-title">실패...</p>
+                    <div class="result-img">
+                        <img :src="require('../assets/fail-img.jpg')" alt="실패">
+                    </div>
+                    <p class="main-title" v-if="failReason =='numberOut'">숫자를 초과하였습니다. <br/>다시 도전하세요 ㅠㅠ</p>
+                    <p class="main-title" v-else-if="failReason =='timeOut'">시간을 초과하였습니다. <br/>다시 도전하세요 ㅠㅠ</p>       
+                </div>
+            </div>     
+            <div class="button-box">
+                <button @click="gameStart($event)" data-setting="fail" class="button">다시 시작!</button>
+            </div>
+        </section>
+    </div>
 </template>
-
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+    import '../style.css'
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+    export default{
+        data(){
+            return{
+                count: 0,
+                goalCount: Math.floor(Math.random()*(50 - 25 ) + 25), //25에서 50 사이
+                duration: 30,
+                currentTime: 0,
+                buttonName: "강아지",
+                step: 1,
+                level: 1,
+                failReason: null
+            }
+        },
+        methods: {
+            gameStart(event){ //게임 시작 이벤트
+                this.step = 2
+
+                let timer = setInterval(() => {
+                    this.currentTime++
+
+                    if( this.currentTime === this.duration ){ //시간 도달
+                        clearInterval(timer)
+
+                        if(this.count === this.goalCount){ //성공
+                            this.step = '3_success'
+                            this.level++
+                            this.failReason = null
+
+                        }else{ //시간 초과
+                            this.step = '3_fail'
+                            this.failReason = 'timeOut'
+                            
+                        }
+                    }else if(this.currentTime < this.duration && this.count > this.goalCount){ //제한 시간 전, 숫자 초과
+                        clearInterval(timer)
+
+                        this.step = '3_fail'
+                        this.failReason = 'numberOut'
+                    }
+                }, 1000)
+
+                if(event.target.getAttribute("data-setting") !=="init"){
+                    this.count = 0
+                    this.currentTime = 0
+                    this.failReason = null
+                    this.goalCount = Math.floor(Math.random()*(50 - 25 ) + 25)
+                }
+            },            
+            counter(){ //게임 내 클릭 버튼 이벤트
+                this.count++
+            }
+        }
+    }
+</script>
